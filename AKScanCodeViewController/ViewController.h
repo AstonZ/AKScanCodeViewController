975e698ae9d881054c187929abdcfd7faf0a05e3
Aston Zhang <liangziokay@gmail.com>
2015-11-17 09:50:57 +0800

    Initial Commit

---
+++ AKScanCodeViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AKScanCodeViewController
+//
+//  Created by 张良 on 15/11/17.
+//  Copyright © 2015年 Aston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AKScanCodeViewController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AKScanCodeViewController
+//
+//  Created by 张良 on 15/11/17.
+//  Copyright © 2015年 Aston. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
